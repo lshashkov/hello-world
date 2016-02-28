7ec9b07e6d2231d83a47b463d420e43482cf721b
lshashkov <leshashkov@gmail.com>
2016-02-28 18:59:48 +0300

    Create

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,24 @@
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+public class Main{
+	
+	public static void main (String[] args) {
+		System.out.println("Hello world!!!");
+	}
+
+}
